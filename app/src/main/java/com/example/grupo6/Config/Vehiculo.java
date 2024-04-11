ef6276f4e61cd9c1fc5414e0f38a7a7e5f489533
Carlos Artica <bayardoartica@gmail.com>
2024-04-11 04:17:14 -0600

    animo

---
+++ app/src/main/java/com/example/grupo6/Config/Vehiculo.java
@@ -0,0 +1,86 @@
+package com.example.grupo6.Config;
+
+import java.io.Serializable;
+
+import java.io.Serializable;
+
+public class Vehiculo implements Serializable {
+    private String marca;
+    private String modelo;
+    private String anio;
+    private String color;
+    private String combustible;
+    private String placa;
+    private String userId;
+
+    public Vehiculo() {
+        // Constructor vacío requerido por Firestore
+    }
+
+    public Vehiculo(String marca, String modelo, String anio, String color, String combustible, String placa, String userId) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anio = anio;
+        this.color = color;
+        this.combustible = combustible;
+        this.placa = placa;
+        this.userId = userId; // Asignar el ID del usuario
+    }
+
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getAnio() {
+        return anio;
+    }
+
+    public void setAnio(String anio) {
+        this.anio = anio;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getCombustible() {
+        return combustible;
+    }
+
+    public void setCombustible(String combustible) {
+        this.combustible = combustible;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
